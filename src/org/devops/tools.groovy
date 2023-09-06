a42a5281fcee70a2d6e293ed134f092118f00fb6
fredy1006 <129719648+fredy1006@users.noreply.github.com>
2023-09-06 14:58:25 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,15 +1,12 @@
 package org.devops
 
-//打印内容
-//def PrintMes(content){
-//    println(content)
-//}
-
 //颜色输出
 def PrintMes(value,color){
-    colors = ['red'    : "\033[30m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033[0m",
+    colors = ['red'    : "\033[31m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033[0m",
               'yellow' : "\033[33m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m",
+              'blue'   : "\033[46m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m",
               'green'  : "\033[32m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m"]
+
     ansiColor('xterm') {
         println{colors[color]}
     }
