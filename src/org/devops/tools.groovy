d11fa4a687240ca8e12ba3c6eb202879d88e1e81
fredy1006 <129719648+fredy1006@users.noreply.github.com>
2023-09-06 15:12:20 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -2,12 +2,15 @@ package org.devops
 
 //颜色输出
 def PrintMes(value,color){
-    colors = ['red'    : "\e[31m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \e[0m\n",
-              'yellow' : "\e[33m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \e]0m\n",
-              'blue'   : "\e[46m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \e]0m\n",
-              'green'  : "\e[32m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \e]0m\n"]
+    colors = ['red'    : "\033[31m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033[0m\n",
+              'yellow' : "\033[33m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m\n",
+              'blue'   : "\033[46m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m\n",
+              'green'  : "\033[32m >>>>>>>>>>>>>>>${value}<<<<<<<<<<<<<<< \033]0m\n"]
 
     ansiColor('xterm') {
         println{colors[color]}
     }
 }
+printf "\e[31mHello\e[0m\n"
+printf "\033[31mHello\033[0m\n"
+printf "\x1b[31mHello\x1b[0m\n"
