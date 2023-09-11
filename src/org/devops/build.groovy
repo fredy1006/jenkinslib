755ef0320ab4685885e2c31397f5dede54f60fcc
fredy1006 <129719648+fredy1006@users.noreply.github.com>
2023-09-11 18:05:20 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,6 +6,7 @@ def Build(buildType,buildShell){
   def buildTools =  ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
   println("当前的构建类型为${buildType}")
   buildHome= tool buildTools[buildType]
+  println("当前构建类型HOME目录为${buildHome}")
 
-  sh "${buildHome}/bin/${buildType} ${buildSHell}"
+  sh "${buildHome}/bin/${buildType} ${buildShell}"
 }
